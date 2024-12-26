00b09eb971a212ce7d1303c5e13cb654ba167389
Palash Bansal <palashbansal96@gmail.com>
2024-12-26 14:29:18 +0530

    Add files icon in example.

--- packages/chonky/example/index.tsx
+++ packages/chonky/example/index.tsx
@@ -1,12 +1,21 @@
 // import 'react-app-polyfill/ie11';
 import * as React from 'react';
-import {FullFileBrowser} from '../dist/index.mjs';
+import {FullFileBrowser, setChonkyDefaults, IconFA} from '../.';
 import {createRoot} from "react-dom/client";
 
+const files = [
+    { id: "lht", name: "Projects", isDir: true },
+    {
+        id: "mcd",
+        name: "chonky-sphere-v2.png",
+        thumbnailUrl: "https://chonky.io/chonky-sphere-v2.png",
+    },
+];
+setChonkyDefaults({ iconComponent: IconFA });
 const App = () => {
   return (
     <div style={{ height: 400 }}>
-      <FullFileBrowser />
+      <FullFileBrowser files={files} />
     </div>
   );
 };
