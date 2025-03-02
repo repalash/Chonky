7d5112f1154f2b48696bf2bd7b7410b90edf0001
ya-nish <yadav.nishant.1907@gmail.com>
2025-03-03 03:01:10 +0530

    Add truncate functionality in case folder chain gets too long

--- packages/chonky/src/components/external/FileNavbar.tsx
+++ packages/chonky/src/components/external/FileNavbar.tsx
@@ -6,15 +6,7 @@
 
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-import React, { ReactElement, useMemo } from 'react';
-import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownSection,
-  DropdownItem
-} from "@heroui/dropdown";
-import { Button }from "@heroui/button"
+import React, { ReactElement, useMemo, useState, useEffect, useRef } from 'react';
 import { important, makeGlobalChonkyStyles } from '../../util/styles';
 import { useFolderChainItems } from './FileNavbar-hooks';
 import { FolderChainButton } from './FolderChainButton';
@@ -27,27 +19,76 @@ export interface FileNavbarProps { }
 export const FileNavbar: React.FC<FileNavbarProps> = React.memo(() => {
   const classes = useStyles();
   const folderChainItems = useFolderChainItems();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [shouldTruncate, setShouldTruncate] = useState(false);
+  
+  useEffect(() => {
+    setShouldTruncate(folderChainItems.length > 3);
+  }, [folderChainItems.length]);
 
   const folderChainComponents = useMemo(() => {
     const components: ReactElement[] = [];
-    for (let i = 0; i < folderChainItems.length; ++i) {
-      const key = `folder-chain-${i}`;
-      const component = (
+    
+    if (!shouldTruncate || folderChainItems.length <= 3) {
+      for (let i = 0; i < folderChainItems.length; ++i) {
+        const key = `folder-chain-${i}`;
+        const component = (
+          <FolderChainButton
+            key={key}
+            first={i === 0}
+            current={i === folderChainItems.length - 1}
+            item={folderChainItems[i]}
+          />
+        );
+        components.push(component);
+      }
+    } else {
+      components.push(
         <FolderChainButton
-          key={key}
-          first={i === 0}
-          current={i === folderChainItems.length - 1}
-          item={folderChainItems[i]}
+          key="folder-chain-0"
+          first={true}
+          current={false}
+          item={folderChainItems[0]}
         />
       );
-      components.push(component);
+      
+      const truncatedItem = {
+        file: null,
+        disabled: false,
+        onClick: () => {
+          // TODO: implement a dropdown to show all hidden items
+          // For now, clicking will just expand the full path temporarily
+          setShouldTruncate(false);
+          setTimeout(() => setShouldTruncate(folderChainItems.length > 3), 5000);
+        },
+      };
+      
+      components.push(
+        <FolderChainButton
+          key="folder-chain-truncated"
+          first={false}
+          current={false}
+          item={truncatedItem}
+          truncated={true}
+        />
+      );
+
+      components.push(
+        <FolderChainButton
+          key={`folder-chain-${folderChainItems.length - 1}`}
+          first={false}
+          current={true}
+          item={folderChainItems[folderChainItems.length - 1]}
+        />
+      );
     }
+    
     return components;
-  }, [folderChainItems]);
+  }, [folderChainItems, shouldTruncate]);
 
   return (
     <Box className={classes.navbarWrapper}>
-      <Box className={classes.navbarContainer}>
+      <Box className={classes.navbarContainer} ref={containerRef}>
         <Breadcrumbs className={classes.navbarBreadcrumbs} separator={<BreadCrumbsSeperator/>}>
           {folderChainComponents}
         </Breadcrumbs>
@@ -67,13 +108,13 @@ const useStyles = makeGlobalChonkyStyles((theme) => ({
     alignItems: 'center',
     paddingTop: '5px',
     paddingBottom: '15px',
-    // borderBottom: '1px solid $gray'
   },
   navbarContainer: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    flex: 1
+    flex: 1,
+    overflow: 'hidden',
   },
   upDirectoryButton: {
     fontSize: important(theme.toolbar.fontSize),
@@ -83,6 +124,7 @@ const useStyles = makeGlobalChonkyStyles((theme) => ({
   navbarBreadcrumbs: {
     fontSize: important(theme.toolbar.fontSize),
     flexGrow: 1,
+    whiteSpace: 'nowrap',
   },
   controlsContainer: {
     display: 'flex',
