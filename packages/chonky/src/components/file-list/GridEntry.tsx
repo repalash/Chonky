98f8093b3c722c10d8eeed198dff99c0ae17c94c
Krishna Nand Patel <imkrishna8737@gmail.com>
2025-03-10 22:36:46 +0530

    push check
    Signed-off-by: Krishna Nand Patel <imkrishna8737@gmail.com>

--- packages/chonky/src/components/file-list/GridEntry.tsx
+++ packages/chonky/src/components/file-list/GridEntry.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { FileEntryProps } from '../../types/file-list.types';
 import { FileHelper } from '../../util/file-helper';
-import { c, makeLocalChonkyStyles } from '../../util/styles';
+import { c, makeLocalChonkyStyles, useIsMobileBreakpoint } from '../../util/styles';
 import { useFileEntryHtmlProps, useFileEntryState } from './FileEntry-hooks';
 import { FileEntryName } from './FileEntryName';
 import { FileEntryState, GridEntryPreviewFile, GridEntryPreviewFolder } from './GridEntryPreview';
